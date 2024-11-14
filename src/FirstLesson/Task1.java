e23288ae38794f722ce7a471062e8891cf436cf6
Kirill <larkin.kirill@mail.ru>
2024-11-14 17:57:10 +0300

    exception #1 commit

--- src/FirstLesson/Task1.java
+++ src/FirstLesson/Task1.java
@@ -1,35 +1,38 @@
 package FirstLesson;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in);) {
+            System.out.println("Введите размер массива");
+            int capacity = getValidatedInt(scanner);
 
-        System.out.println("Введите размер массива");
-        int capacity = scanner.nextInt();
-        if (capacity <= 0) {
-            throw new RuntimeException("Размер массива должен быть больше нуля");
+            System.out.println("Введите нижнюю и верхнюю границы генерации случайных чисел");
+
+            int numb1 = scanner.nextInt();
+            int numb2 = scanner.nextInt();
+            int minEdge = Math.min(numb1, numb2);
+            int maxEdge = Math.max(numb1, numb2);
+
+            int[] arrayInt = getRandomIntArray(capacity, minEdge, maxEdge);
+            double[] arrayDouble = getRandomDoubleArray(capacity, minEdge, maxEdge);
+
+            printMax(arrayInt);
+            printMin(arrayInt);
+            printAverage(arrayInt);
+            sort(arrayInt);
+            sortDescending(arrayInt);
+
+            printMax(arrayDouble);
+            printMin(arrayDouble);
+            printAverage(arrayDouble);
+            sort(arrayDouble);
+            sortDescending(arrayDouble);
+        } catch (Exception e) {
+            System.out.println("Непредвиденная ошибка");
         }
-
-        System.out.println("Введите нижнюю и верхнюю границы генерации случайных чисел");
-        int minEdge = scanner.nextInt();
-        int maxEdge = scanner.nextInt();
-
-        int[] arrayInt = getRandomIntArray(capacity,minEdge,maxEdge);
-        double[] arrayDouble = getRandomDoubleArray(capacity, minEdge, maxEdge);
-
-        printMax(arrayInt);
-        printMin(arrayInt);
-        printAverage(arrayInt);
-        sort(arrayInt);
-        sortDescending(arrayInt);
-
-        printMax(arrayDouble);
-        printMin(arrayDouble);
-        printAverage(arrayDouble);
-        sort(arrayDouble);
-        sortDescending(arrayDouble);
     }
 
     public static int[] getRandomIntArray(int capacity, int min, int max) {
@@ -155,4 +158,34 @@ public class Task1 {
         }
         return array;
     }
+
+    public static int getValidatedInt(Scanner scanner) {
+        int result;
+        while (true){
+            try {
+                result = scanner.nextInt();
+                return result;
+            } catch (InputMismatchException e) {
+                System.out.println("Неправиьный ввод. Введите целое число.");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    public static int getArraySize(Scanner scanner) {
+        int result;
+        while (true){
+            try {
+                result = getValidatedInt(scanner);
+                if (result > 0) {
+                    return result;
+                } else throw new ArrayIndexOutOfBoundsException();
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Размер массива должен быть больше нуля");
+                scanner.nextLine();
+            }
+        }
+    }
+
+
 }
